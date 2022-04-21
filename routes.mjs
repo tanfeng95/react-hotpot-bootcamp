f6a0276412b7a8d89520ef6bbe495a0dd161f60d
Kai Yuan Neo <kai@rocketacademy.co>
2022-04-21 11:33:14 +0800

    Update primary route to be / instead of /home for convenience

--- routes.mjs
+++ routes.mjs
@@ -1,9 +1,8 @@
 import { resolve } from 'path';
-import db from './models/index.mjs';
 
 export default function routes(app) {
   // special JS page. Include the webpack index.html file
-  app.get('/home', (request, response) => {
+  app.get('/', (request, response) => {
     response.sendFile(resolve('dist', 'main.html'));
   });
 }
